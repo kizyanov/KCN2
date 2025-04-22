e5d6141ce5d1cf4e148d094fbd54a49cc3171f95
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2025-04-22 22:26:36 +0300

    .

--- src/main.py
+++ src/main.py
@@ -1615,11 +1615,10 @@ class KCN:
     ) -> Result[None, Exception]:
         """Cancel all order in data list."""
         for order in data:
-            for order_id, symbol in order.values():
-                await self.delete_api_v3_hf_margin_orders(
-                    order_id,
-                    {"symbol": symbol},
-                )
+            await self.delete_api_v3_hf_margin_orders(
+                order["id"],
+                {"symbol": order["symbol"]},
+            )
         return Ok(None)
 
     def get_msg_for_subscribe_matching(
