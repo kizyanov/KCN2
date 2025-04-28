0e7ff7f363adaa58fb129907e951baafc186a1ba
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2025-04-28 14:45:42 +0300

    .

--- src/main.py
+++ src/main.py
@@ -1452,10 +1452,7 @@ class KCN:
                     case Ok(api_v3_margin_accounts):
                         for account in api_v3_margin_accounts.data.accounts:
                             if symbol_name == account.currency:
-                                if (
-                                    data.side == "buy"
-                                    and Decimal(account.liability) != 0
-                                ):
+                                if Decimal(account.liability) != 0:
                                     # check exist liabilities
                                     match await do_async(
                                         Ok(_)
@@ -1465,13 +1462,11 @@ class KCN:
                                     ):
                                         case Err(exc):
                                             logger.exception(exc)
-                                elif data.side == "sell":
+
+                                if data.side == "sell":
                                     # create new orders
                                     match await do_async(
                                         Ok(_)
-                                        for _ in await self.make_buy_margin_order(
-                                            symbol_name
-                                        )
                                         for _ in await self.make_sell_margin_order(
                                             symbol_name
                                         )
